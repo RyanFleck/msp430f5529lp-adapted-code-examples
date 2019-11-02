3793b5568e8fd9552bacfeccbcf2ca85686903b5
Ryan Fleck <ryan.fleck@protonmail.com>
2019-11-01 20:31:10 -0400

    Add additional info on pull-up resistors.

--- MSP430-3-EnableAndDisableButtons.c
+++ MSP430-3-EnableAndDisableButtons.c
@@ -2,8 +2,10 @@
 #include <stdbool.h>
 
 /**
- * EnableAndDisableButtons.c
- * Author: Ryan Fleck - Ryan.Fleck@protonmail.com
+ * EnableAndDisableButtons.c - Polling edition.
+ * Adapted by Ryan Fleck - Ryan.Fleck@protonmail.com
+ *
+ * Blinks LED2 at P4.7
  */
 
 void main(void)
@@ -16,19 +18,17 @@ void main(void)
     // Set the seventh bit of the P4 Data Direction Register to HIGH
     // This allows us to use P4.7 LED as an OUTPUT.
     P4DIR |= BIT7;
+    P1DIR |= BIT0;
 
     // Button setup:
-    P1DIR &= ~BIT1; // P1.1 set to input. Other P1 bits unaffected.
-    // I understand that we need to enable and do something with the
-    // pulldown resistor, but I need to read more to see exactly what's
-    // going on with these two steps.
+    P1DIR &= ~BIT1;
     P1OUT |= BIT1;
     P1REN |= BIT1;
 
     // Second button setup
-    P2DIR |= BIT1;
-    P2OUT |= BIT1;
-    P2REN |= BIT1;
+    P2DIR &= ~BIT1; // Bit 1 of Register 2 is zero (input)
+    P2OUT |= BIT1;  // Pull-up mode is PULL-UP to 5v.
+    P2REN |= BIT1;  // Enable internal pull-up.
 
     while (1)
     {
@@ -37,15 +37,19 @@ void main(void)
         // When bitwise AND between P1IN and BIT1 is NOT one, we want to activate this.
         if (!(P1IN & BIT1))
         {
-            // Enable the LED by toggling bit 7 ON.
+            // Enable the LED2 by toggling bit 7 ON.
             P4OUT |= BIT7;
+            // Disable LED1
+            P1OUT &= ~BIT0;
         }
         else if(!(P2IN & BIT1))
         {
-            // Disable the LED by toggling bit 7 OFF,
+            // Disable the LED2 by toggling bit 7 OFF,
             // What this really does is AND with the 1C of BIT7, allowing
             // everything but BIT7 to stay on in the P4OUT register.
             P4OUT &= ~BIT7;
+            // Enable the LED1
+            P1OUT |= BIT0;
         }
 
     }
