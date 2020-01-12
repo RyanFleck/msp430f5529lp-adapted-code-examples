65631e12b081f62a111451b5b4c2f01e7dd9c7e0
Ryan Fleck <ryan.fleck@protonmail.com>
2020-01-12 10:28:55 -0500

    Correct low-power mode, 1 Hz blinks.

--- MSP430-5-TimerA0.c
+++ MSP430-5-TimerA0.c
@@ -4,7 +4,12 @@
  * Timer experiments.
  * Adapted by Ryan Fleck - Ryan.Fleck@protonmail.com
  *
- * Switch at P1.1 blinks LED2 at P4.7
+ * Blinks LED2 at P4.7
+ *
+ * 2^16 = 65536
+ * ~1MHz clock /8 = 125000 Hz
+ * * 65536 ticks = 1.9 Hz blinks
+ * + external prescaler /2 => ~1 Hz blinks.
  */
 
 void main(void)
@@ -19,13 +24,15 @@ void main(void)
     P4OUT &= ~BIT7; // Turns LED off.
 
     // Configure Timer A0.
-    TA0CTL = TASSEL_2 + MC_2 + TACLR;
+    TA0CTL = TASSEL_2 + MC_2 + TACLR + ID_3;
     // From TI example ta0_02:
     // - SMCLK set as clock.
     // - MC - mode control - timer counts up to FFFF and resets.
     // - Clears TAR, previous count, clock divider.
-    TA0CTL |= BIT7 + BIT6;
     // - Sets prescaler to 8.
+
+    TA0EX0 = TAIDEX_1;
+    // - Sets external prescaler to 2, total of 16
 
     // Configure Timer A0 Channel 0:
     //TA0CCR0 = 0xFFFF;
@@ -39,11 +46,9 @@ void main(void)
     TA0CCTL2 = CCIE;
     TA0CCR2 = 0xFFFA;
 
-    // Enable all interrupts.
-    __bis_SR_register(GIE);
+    // Wait for timer interrupts.
+    _BIS_SR(LPM3_bits + GIE);
 
-    while (1)
-        ;
 }
 
 /* TimerA has two interrupt vectors:
@@ -54,14 +59,14 @@ void main(void)
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void TIMER0_A0_ISR(void)
 {
-
+    // Handle CCR0 interrupts.
 }
 
 #pragma vector=TIMER0_A1_VECTOR
 __interrupt void TIMER0_A1_ISR(void)
 {
-    P1OUT ^= 0x01;                            // Toggle P1.0
-    // Allows the compiler to generate more efficient code for the switch.
+    P1OUT ^= 0x01; // Toggle P1.0
+    // Allows the compiler to generate more efficient code for the switch, equivalent to switch(TA0IV)
     // source: https://e2e.ti.com/support/microcontrollers/msp430/f/166/t/557522?What-does-the-even-in-range-function-do-
     switch (__even_in_range(TA0IV, TA0IV_TAIFG))
     {
